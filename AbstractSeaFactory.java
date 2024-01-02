4146fca2b4fedc90259e37c0a17dd44e6a1285ed
Shay Finegold <73387008+shayfine@users.noreply.github.com>
2024-01-02 05:50:36 +0200

    Add files via upload

---
+++ AbstractSeaFactory.java
@@ -0,0 +1,12 @@
+
+/**
+ * interface with one method to create sea creature :swimmable\immobile object
+ * @author Adir Zoari 203002753
+ * @author Idan Levi 305562431
+ */
+
+public interface AbstractSeaFactory {
+
+	public SeaCreature produceSeaCreature(String type);
+
+}
