f650c95af864642a33acb7e5a07f265aa4b1a083
Shay Finegold <73387008+shayfine@users.noreply.github.com>
2024-01-02 05:55:02 +0200

    Add files via upload

---
+++ SeaCreature.java
@@ -0,0 +1,13 @@
+/**
+ * seaCreatue is interface that swimmable and immobile classes implements and return object 
+ * ---abstract factory design pattern----
+ * @author Adir Zoari 203002753
+ * @author Idan Levi 305562431
+ */
+
+import java.awt.Graphics;
+
+public interface SeaCreature {
+	public void drawCreatrue(Graphics g);
+
+}
